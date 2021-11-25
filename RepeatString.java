21c10df5ca1b6e788728c8ec93b35fdeafab8fef
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2021-11-25 13:24:54 -0600

    Create RepeatString.java

---
+++ RepeatString.java
@@ -0,0 +1,11 @@
+// open in algorithms folder
+public class algorithms {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "abc";
+		
+		System.out.println( str.repeat(3) );
+	}
+
+}
