ccb84c612d6c0323bb323a7eaa5d3d5d3f4c7d04
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2021-12-02 10:37:57 -0600

    Create 4_printArray.java

---
+++ 4_printArray.java
@@ -0,0 +1,15 @@
+package algorithms;
+
+public class algorithms {
+
+	public static void main(String[] args) {
+		String array[] = {"My", "name", "is", "Zelma"};
+		
+		// loop through and print contents
+		for(int i = 0; i < array.length; i++) {
+			System.out.println(array[i]);
+		}
+		
+	} // end of main method
+
+} // end of class
