577958b76aa94ef99f532b006c5516d1ec398217
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2021-12-02 12:15:15 -0600

    Create 10_confirmEnding.java

---
+++ 10_confirmEnding.java
@@ -0,0 +1,13 @@
+package algorithms;
+
+public class algorithms {
+
+	public static void main(String[] args) {
+		String str = "This is a test string";
+		
+		boolean var1 = str.endsWith("string");
+		
+		System.out.println(var1);
+	} // end of main method
+
+} // end of class
