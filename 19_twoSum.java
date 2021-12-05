efa1cd257ce6632647f9ad212dc21ec70c30e9f8
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2021-12-05 12:19:23 -0600

    Update 19_twoSum.java

--- 19_twoSum.java
+++ 19_twoSum.java
@@ -1,23 +1,27 @@
-// create a method that finds the largest element in a given array of ints
-public class Practice {
-    public int[] twoSum(int[] nums, int target) {
-        for(int i = 0; i < nums.length - 1; i++)
-            for(int j = i + 1; j < nums.length; j++)
-                if(nums[i] + nums[j] == target)
-                    return new int[]{i, j};
-                    return new int[2]; 
+class Solution {
+    void twoSum(int[] nums, int target) {
+        int[] res = new int[2];
+        int sum=0;
+        for(int i=0; i < nums.length; i++){
+            for(int j=(i+1); j < nums.length; j++){
+                sum = nums[i] + nums[j];
+                if(sum==target){
+                    res[0]=nums[i];
+                    res[1]=nums[j];
+                }
+            }
+        }
+        
+        for(int h = 0; h < nums.length-1; h++) {
+            System.out.print(res[h]);
+        }
     }
 
-	public static void main(String[] args) {  // main block
-        Practice p = new Practice();
+    public static void main(String[] args) {
+        Solution s = new Solution();
 
-        int myArr[] = {1,2,3,4,5,6};
-        int newArr[] = p.twoSum(myArr, 3);
+        int myArr[] = {1,2,3};
+        s.twoSum(myArr, 5);
 
-        for(int i = 0; i < newArr.length; i++) {
-            System.out.println(newArr[i]);
-        }
-    
-    } // end of main block
-
-} // end of class
+    }
+}
