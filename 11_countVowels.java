3bc8665314ed2bec76c0d5335354a8bbbd620d53
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2021-12-06 10:49:22 -0600

    Update 11_countVowels.java

--- 11_countVowels.java
+++ 11_countVowels.java
@@ -1,23 +1,26 @@
-package algorithms;
+public class Practice {
+    // create a method that checks to see how many vowels are in a given string
+    void vowelCount(String str) {
+        // create a count variable
+        int count = 0;
+        // loop through the string, if current char is a vowel
+        for(int i = 0; i < str.length(); i++) {
+            // create a variable called "ch" to hold current char
+            char ch = str.charAt(i);
+            // check to see if current char is a vowel
+            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                count++;
+            }
+        }
 
-public class algorithms {
+        System.out.print(count);
+    }
 
 	public static void main(String[] args) {
-		String str = "Hi there";
-		int count = 0;
-		
-		// loop through str, if current element is a vowel
-		// add to count
-		for(int i = 0; i < str.length(); i++) {
-			char ch = str.charAt(i);
-			if(ch == 'a'|| ch == 'e'|| ch == 'i' ||ch == 'o' ||ch == 'u'){
-	            count ++;
-	         }
-		}
-		
-		System.out.println(count);
-		
-		
+		Practice p = new Practice();
+
+        String myStr = "wow";
+        p.vowelCount(myStr);
 	} // end of main method
 
 } // end of class
