98f2ba14d69c624df40d2a58331ca7beb1f58f06
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2021-12-06 10:41:11 -0600

    Update 10_confirmEnding.java

--- 10_confirmEnding.java
+++ 10_confirmEnding.java
@@ -1,13 +1,15 @@
-package algorithms;
-
-public class algorithms {
+public class Practice {
+    // create a method that finds the first non-repeating letter in a string
+    void confirmEnding(String str, String target) {
+        boolean result = str.endsWith(target);
+        System.out.print(result);
+    }
 
 	public static void main(String[] args) {
-		String str = "This is a test string";
-		
-		boolean var1 = str.endsWith("string");
-		
-		System.out.println(var1);
+		Practice p = new Practice();
+
+        String myStr = "wow";
+        p.confirmEnding(myStr, "e");
 	} // end of main method
 
 } // end of class
