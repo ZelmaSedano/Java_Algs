710a6d30976d83f66afc5635b015d27052d549d1
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2021-12-06 10:17:39 -0600

    Update 8_sortLettersArray.java

--- 8_sortLettersArray.java
+++ 8_sortLettersArray.java
@@ -1,18 +1,18 @@
-package algorithms;
-// !!!! import arrays method from utils !!!!
-import java.util.Arrays;
+// create a method that sorts a given array
+import java.util.Arrays;  // - import Array utils library to use .sort()
 
-public class algorithms {
+public class Practice {
+    // create a method to sort the chars in an array
+    void sortArr(int[] arr) {
+        Arrays.sort(arr);
 
+        System.out.println(Arrays.toString(arr));
+    }
 	public static void main(String[] args) {
-		// array of letters
-		char arr[] = {'i', 'e', 'o', 'u', 'd'};
-		
-		Arrays.sort(arr);
-		
-		for(int i = 0; i < arr.length; i++) {
-			System.out.print(arr[i]);
-		}
+		Practice p = new Practice();
+
+        int[] myArr = {33,3333,333,33,3};
+        p.sortArr(myArr);;
 	} // end of main method
 
 } // end of class
