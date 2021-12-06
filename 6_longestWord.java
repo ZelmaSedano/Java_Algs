f8bf178b9d3a6ba3dc20dd343cfeffa384b9e799
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2021-12-06 10:21:53 -0600

    Update 6_longestWord.java

--- 6_longestWord.java
+++ 6_longestWord.java
@@ -1,25 +1,27 @@
-package algorithms;
+// create a method that sorts a given array
+import java.util.Arrays;  // - import Array utils library to use .sort()
 
-public class algorithms {
+public class Practice {
+    // create a method that finds the longest word in a string
+    void longestWord(String str) {
+        // split the string into a collection of words
+        String[] words = str.split(" ");
+        String longest = ""; // create variable to hold longest word
+
+        // loop through the array of words
+        for(int i = 0; i < words.length; i++) {
+            if(words[i].length() > longest.length()) {
+                longest = words[i];
+            }
+        }
+        System.out.println("Longest word: " + longest);
+    }
 
 	public static void main(String[] args) {
-		// string we're analyzing
-		String str = "Today is a new day";
-		// split the string into an array of words
-		String [] words = str.split(" "); 
-		
-		String longest = "";
-		
-		// loop through the array
-		for(int i = 0; i < words.length; i++) {
-			if(words[i].length() > longest.length()) {
-				longest = words[i];
-			}
-		}
-		
-		System.out.println(longest);
-		
-		
+		Practice p = new Practice();
+
+        String myStr = "hi there";
+        p.longestWord(myStr);
 	} // end of main method
 
 } // end of class
