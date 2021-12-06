2b5f9c68e51b68e66265f5cf883bf3d0c29b87bc
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2021-12-06 10:57:09 -0600

    Update 12_matchingParentheses.java

--- 12_matchingParentheses.java
+++ 12_matchingParentheses.java
@@ -1,29 +1,29 @@
-package algorithms;
+public class Practice {
+    // create a method that checks to see if there is an equal number of opening and closing parentheses
+    void matchingParentheses(String str) {
+        // initialize a count variable
+        int counter = 0; 
+        // error-handling: if length is 0, print not valid
+        if(str.length() == 0) {
+            System.out.println("Please enter parentheses");
+        } else {
+            // using a for loop, check each char
+            // covert string to Array of chars
+            for(char ch : str.toCharArray()) {
+                if(ch == '(') counter++;
+                else if(ch == ')') counter--;
+            }
+        }
 
-public class algorithms {
-
+        if(counter == 0) {
+            System.out.println("equal parentheses!");
+        } else {
+            System.out.println("parenthese aren't equal :( ");
+        }
+    }
 	public static void main(String[] args) {
-		int counter = 0;
-		String str = "()";	// change this code
-		
-		if(str.length() == 0) {
-			System.out.println("not a valid entry");
-		} else {
-			for(char ch : str.toCharArray()) {
-				if(ch == '(') counter++;	// go up 1
-				else if(ch == ')') counter--;	// go down 1
-			}
-			
-			if(counter == 0) {
-				System.out.println("matching parentheses!");
-			} else {
-				System.out.println("not matching :(");
-			}
-		}
-		
-		
-		
-		
+		Practice p = new Practice();
+		p.matchingParentheses(")");
 	} // end of main method
 
 } // end of class
