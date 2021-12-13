9497f661b910bdb0bff9a86232fdefd589ddce5a
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2021-12-13 11:55:14 -0600

    Update 7_reverseString.java

--- 7_reverseString.java
+++ 7_reverseString.java
@@ -1,24 +1,26 @@
-public class Practice {
-    // create a method that reverses a given string
-    void reverseStr(String str) {
-        // create a string variable to hold reversed string
-        String reversed = "";
-        // create a variable to hold length of string, since we can't use str.length() unlike arr.length()
-        int strLength = str.length();
-
-        // loop backwards through string, adding to reversed
-        for(int i = (strLength-1); i >= 0; i--) {
-            reversed += str.charAt(i);
+class Practice {
+    // create a method that reverses and returns a given string
+    public String reverseString(String str) {
+        // if string is empty, return null
+        if (str.length() == 0) {
+            return null;
         }
 
-        System.out.print(reversed);
+        String result = ""; // empty string to add to
+        str = str.replaceAll("\\s+", "");
+        // create a variable to hold the String's length
+        int length = str.length();
+
+        for (int i = length - 1; i >= 0; i--) {
+            result += str.charAt(i);
+        }
+
+        return result;
     }
 
-	public static void main(String[] args) {
-		Practice p = new Practice();
-
-        String myStr = "word";
-        p.reverseStr(myStr);
-	} // end of main method
-
-} // end of class
+    // main method
+    public static void main(String[] args) {
+        Practice p = new Practice();
+        System.out.println(p.reverseString("h   i"));
+    }
+}
