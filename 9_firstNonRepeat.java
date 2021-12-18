f627c824ea1f27e09d4b9de61dc4536a70acd359
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2021-12-18 08:36:25 -0600

    Update 9_firstNonRepeat.java

--- 9_firstNonRepeat.java
+++ 9_firstNonRepeat.java
@@ -1,42 +1,45 @@
+
+// import the util library to use HashMap
 import java.util.*;
 
 class Practice {
-    public char firstNonRepeatingCharacter(String s) {
-        // edge cases
-        if (s == null)
-            return ' ';
-        if (s.length() == 0)
-            return ' ';
+    // create a method that returns the first non-repeating character in a string
+    public char firstNonRepeating(String str) {
+        // take care of edge cases, if length is 0 return -1
+        if (str.length() == 0) {
+            // since it's a char method, we HAVE to return char
+            return '0';
+        }
 
-        // create a hash map, character = key, value = integer
+        // create a HashMap that has Character as key & Integer as Value
         HashMap<Character, Integer> characterCount = new HashMap<>();
 
-        // turn the string into an Array of chars & loop through it, call each char "c"
-        for (char c : s.toCharArray()) {
-            // characterCount is map
-            // if char "c" isn't in hashmap, then add c to hashmap w/ 1 as the value
+        // using an enhanced for loop: convert strint into array of chars
+        for (char c : str.toCharArray()) {
+            // if HashMap characterCount DOESN'T contain key (c)
             if (!characterCount.containsKey(c)) {
+                // then add key c w/ value of 1
                 characterCount.put(c, 1);
-                // if it IS in the counter, get the value for c and add c & its former value
-                // incremented by 1 to HashMap
             } else {
+                // add c and add its former value (acquired using .get()) + 1
                 characterCount.put(c, characterCount.get(c) + 1);
             }
         }
-        // loop through hashmap again and check to see if one of the c's value is 1
-        for (char c : s.toCharArray()) {
+
+        // loop through the HashMap again and check to see if one of c's values is 1
+        for (char c : str.toCharArray()) {
             // if it their is a character w/ 1 as value, return that character
             if (characterCount.get(c) == 1) {
                 return c;
             }
         }
 
-        return ' ';
+        // as a backup, return nothing
+        return '0';
     }
 
-    // main method
     public static void main(String[] args) {
         Practice p = new Practice();
-        System.out.println(p.firstNonRepeatingCharacter("yyola"));
+        System.out.println(p.firstNonRepeating("yola"));
     }
 }
