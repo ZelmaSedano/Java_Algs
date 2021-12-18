70facebf8c34e9fee4fa4106c68d42e8114f1be2
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2021-12-18 08:11:53 -0600

    Update 9_firstNonRepeat.java

--- 9_firstNonRepeat.java
+++ 9_firstNonRepeat.java
@@ -2,20 +2,23 @@ import java.util.*;
 
 class Practice {
     public char firstNonRepeatingCharacter(String s) {
-        // create a hash map
-        HashMap<Character, Integer> characterCount = new HashMap<>();
-        // Best to discuss it with interviewer, what should we return here?
+        // edge cases
         if (s == null)
             return ' ';
         if (s.length() == 0)
             return ' ';
+
+        // create a hash map, character = key, value = integer
+        HashMap<Character, Integer> characterCount = new HashMap<>();
+
         // turn the string into an Array of chars & loop through it, call each char "c"
         for (char c : s.toCharArray()) {
             // characterCount is map
             // if char "c" isn't in hashmap, then add c to hashmap w/ 1 as the value
             if (!characterCount.containsKey(c)) {
                 characterCount.put(c, 1);
-                // if it IS in the counter, add the character & increment its value by 1
+                // if it IS in the counter, get the value for c and add c & its former value
+                // incremented by 1 to HashMap
             } else {
                 characterCount.put(c, characterCount.get(c) + 1);
             }
@@ -23,8 +26,9 @@ class Practice {
         // loop through hashmap again and check to see if one of the c's value is 1
         for (char c : s.toCharArray()) {
             // if it their is a character w/ 1 as value, return that character
-            if (characterCount.get(c) == 1)
+            if (characterCount.get(c) == 1) {
                 return c;
+            }
         }
 
         return ' ';
