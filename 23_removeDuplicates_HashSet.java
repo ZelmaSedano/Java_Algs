d787f17be3b4a86df2f2dfefa698623cca3e7f77
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-01-03 18:08:46 -0600

    Create 23_removeDuplicates_HashSet.java

---
+++ 23_removeDuplicates_HashSet.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Practice {
+    public static void main(String[] args) {
+        ArrayList<String> songList = new ArrayList<String>();
+        songList.add("Pink Moon");
+        songList.add("Somersault");
+        songList.add("Circles");
+        songList.add("Deep Channel");
+        songList.add("Pink Moon");
+        songList.add("Pink Moon");
+        songList.add("Listen");
+        songList.add("Listen");
+
+        HashSet<String> songSet = new HashSet<String>();
+        songSet.addAll(songList);
+        System.out.println(songSet);
+        // all the duplicates are removed
+    }
+}
