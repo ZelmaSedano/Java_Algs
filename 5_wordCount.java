7b5735b094d69774b0f5c9e28f51bd5f781077a2
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-01-05 13:41:19 -0600

    Update 5_wordCount.java

--- 5_wordCount.java
+++ 5_wordCount.java
@@ -1,16 +1,20 @@
-class Practice {
-    // create a method that finds the number of words in a given string
-    void wordCount(String str) {
-        // split string into an array of words
+import java.util.*;
+
+public class Practice {
+    // create a method that returns the number of words in a string
+    // hint: you will split the string into a collection of words (Array) & return
+    // length
+    public int wordCount(String str) { // takes a string as an argument
+        // split the string into a collection of words & save in array called "words"
         String[] words = str.trim().split(" ");
 
-        System.out.println(words.length);
+        return words.length;
     }
 
-    // main method
+    // main method where new Objects are created & methods are called
     public static void main(String[] args) {
         Practice p = new Practice();
 
-        p.wordCount("hi there");
+        System.out.println(p.wordCount("hi there yo there ya da"));
     }
 }
