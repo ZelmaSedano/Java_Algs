d397366a6f66f67651c9ad93c1e4184526d11199
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-01-05 13:46:46 -0600

    Update 10_confirmEnding.java

--- 10_confirmEnding.java
+++ 10_confirmEnding.java
@@ -1,12 +1,17 @@
-class Practice {
-    // create a method that checks to see if a given string ends w/ target
+import java.util.*;
+
+public class Practice {
+    // create a method that checks to see if a given string ends w/ a given target
+    // string --> ex: "tree", "ee" --> true
     public boolean confirmEnding(String str, String target) {
         return str.endsWith(target);
     }
 
-    // main method
+    // main method where new Objects are created & methods are called
     public static void main(String[] args) {
+        // new object
         Practice p = new Practice();
-        System.out.println(p.confirmEnding("hi", "i"));
+
+        System.out.println(p.confirmEnding("hi", "e"));
     }
 }
