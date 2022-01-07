f9cef0957f27ce0a9d2c9cf99b55d3d8d6401ce9
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-01-07 10:05:21 -0600

    Create 32_filterSortAndPrintNames.java

---
+++ 32_filterSortAndPrintNames.java
@@ -0,0 +1,15 @@
+import java.util.stream.*;
+// you need the util library b/c you are using Arrays
+import java.util.*;
+
+public class Practice {
+    public static void main(String[] args) {
+        // Stream from Array, sort, filter & print
+        String[] names = { "Al", "Ankit", "Kushal", "Shivika", "Sarah" };
+        Arrays.stream(names)
+                // filters names that start with "S"
+                .filter(x -> x.startsWith("S"))
+                .sorted()
+                .forEach(System.out::println);
+    }
+}
