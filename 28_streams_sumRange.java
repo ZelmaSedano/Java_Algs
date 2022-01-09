bd2925adc090326af4b05f1942eb05a623223030
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-01-09 08:25:51 -0600

    Update 28_streams_sumRange.java

--- 28_streams_sumRange.java
+++ 28_streams_sumRange.java
@@ -1,16 +1,13 @@
-import java.util.Arrays;
-import java.util.List;
 import java.util.stream.*;
 
 public class Practice {
 
     public static void main(String[] args) {
+        // print out the results by nesting everything inside a System.out statement
         System.out.println(
                 IntStream
-                        // non-inclusive: 1+2+3+4 = 10
-                        .range(1, 5)
+                        .range(1, 5) // stops at 4, 5 is non-inclusive
                         .sum());
-
     }
 
 }
