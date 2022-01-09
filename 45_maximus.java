ca92ee8d73ce91b4e7df9a75b4df860ebfc10887
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-01-09 09:56:00 -0600

    Create 45_maximus.java

---
+++ 45_maximus.java
@@ -0,0 +1,15 @@
+import java.util.stream.*;
+import java.util.*; // imports List
+
+public class Practice {
+    public static void main(String[] args) {
+        // create a List of integers & call it nums
+        List<Integer> nums = List.of(1, 2, 3, 4, 5);
+
+        int minimus = nums.stream()
+                .max(Integer::compare)
+                .get();
+
+        System.out.println(minimus);
+    }
+}
