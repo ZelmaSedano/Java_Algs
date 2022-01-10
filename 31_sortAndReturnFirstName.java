4d5b7785fb3d88e72f8e28ad6d2b89ad8add300d
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-01-10 08:14:16 -0600

    Update 31_sortAndReturnFirstName.java

--- 31_sortAndReturnFirstName.java
+++ 31_sortAndReturnFirstName.java
@@ -1,13 +1,14 @@
 import java.util.stream.*;
+import java.util.*; // imports Arrays
 
 public class Practice {
     public static void main(String[] args) {
-        // you can use Stream.of to create a stream of custom data you pass in
-        // in this example, we're passing in 3 strings, sorting them, and returning the
-        // first one
-        Stream.of("Ava", "Aneri", "Alberto")
+        // create a stream of Names
+        String result = Stream.of("Ava", "Aneri", "Alberto")
                 .sorted()
                 .findFirst()
-                .ifPresent(System.out::println);
+                .get();
+
+        System.out.println(result);
     }
 }
