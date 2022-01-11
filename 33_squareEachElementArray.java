a31495c2c9bdcc908ca84a154eb44576cdcb7e9f
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-01-11 08:42:57 -0600

    Update 33_squareEachElementArray.java

--- 33_squareEachElementArray.java
+++ 33_squareEachElementArray.java
@@ -4,11 +4,8 @@ import java.util.stream.*;
 public class Practice {
     public static void main(String[] args) {
         // create a stream of an Array (Arrays)
-        Arrays.stream(new int[] { 2, 4, 6, 8, 10 })
-                // map over each element & square it
-                .map(number -> number * number)
-                // print out each element
-                .forEach(number -> System.out.println(number));
-
+        Arrays.stream(new int[] { 1, 2, 3, 4, 5 })
+                .map(num -> num * num)
+                .forEach(num -> System.out.println(num));
     }
 }
