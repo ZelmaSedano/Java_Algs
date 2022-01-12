769001c80c7b96d776f981db56c97ef5ba62dc55
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-01-12 09:09:52 -0600

    Update 33_squareEachElementArray.java

--- 33_squareEachElementArray.java
+++ 33_squareEachElementArray.java
@@ -1,11 +1,18 @@
-import java.util.*;
+import java.util.*; // import List
 import java.util.stream.*;
 
 public class Practice {
     public static void main(String[] args) {
-        // create a stream of an Array (Arrays)
-        Arrays.stream(new int[] { 1, 2, 3, 4, 5 })
-                .map(num -> num * num)
-                .forEach(num -> System.out.println(num));
+        // create a List of integers
+        List<Integer> nums = List.of(1, 2, 3, 4, 5, 6);
+
+        // create a variable that's a list & set it to nums.stream()
+        List<Integer> squares = nums.stream()
+                // apply .map to square each element, then collect into List
+                .map(x -> x * x)
+                .collect(Collectors.toList());
+
+        System.out.println(squares);
+
     }
 }
