d8792d3419a8ad4d0c7ebb978f54890a4786b902
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-01-12 11:21:46 -0600

    Update 35_filterListOfPeople(lowerCase).java

--- 35_filterListOfPeople(lowerCase).java
+++ 35_filterListOfPeople(lowerCase).java
@@ -1,16 +1,17 @@
 import java.util.stream.*;
-// you need the util library b/c you are using Arrays
 import java.util.*;
 
-public class Practice {
-    public static void main(String[] args) {
-        List<String> people = Arrays.asList("Al", "Ankit", "Sarika", "Hans", "Eric");
+public class Practice1 {
 
-        people
-                // creates a stream based off the people
-                .stream()
-                .map(String::toLowerCase)
-                .filter(x -> x.startsWith("a"))
-                .forEach(System.out::println);
-    }
+  public static void main(String[] args) {
+    List<String> people = List.of("Al", "Ankit", "Sarika", "Hans", "Eric");
+
+    List<String> lowerCase = people.stream()
+        .map(String::toLowerCase)
+        .filter(x -> x.startsWith("a"))
+        .collect(Collectors.toList());
+
+    System.out.println(lowerCase);
+
+  }
 }
