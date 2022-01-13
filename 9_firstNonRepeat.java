33797797601f455550a889d2b4e4b5a86e18717b
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-01-13 09:42:43 -0600

    Update 9_firstNonRepeat.java

--- 9_firstNonRepeat.java
+++ 9_firstNonRepeat.java
@@ -1,32 +1,36 @@
 import java.util.stream.*;
 import java.util.*;
-import java.util.Map.Entry; // imports Map.Entry
+import java.util.Map.Entry;
 
-public class Practice1 {
-  public void firstNonRepeat(String str) {
-    // create a LinkedHashMap, because that keeps the order intact
-    Map<Character, Integer> charCount = new LinkedHashMap<>(str.length());
+public class Practice {
+    // make a method that returns the first letter that's not repeated in the String
+    public void firstNonRepeat(String str) {
+        if (str.length() == 0) {
+            System.out.println("please enter a valid string");
+        }
 
-    // using enhanced for loop, loop through the string & convert it to an Array of
-    // chars
-    for (char c : str.toCharArray()) {
-      // if HashMap contains c, then make value previous value + 1
-      // if it doens't containe c, then add and make value 1
-      charCount.put(c, charCount.containsKey(c) ? charCount.get(c) + 1 : 1);
+        // make a LinkedHashMap
+        Map<Character, Integer> charCount = new LinkedHashMap<>(str.length());
+
+        // convert the string to an Array of chars & loop through it
+        for (char c : str.toCharArray()) {
+            charCount.put(c, charCount.containsKey(c) ? charCount.get(c) + 1 : 1);
+        }
+
+        // loop through the map using Entry<Character,Integer> & call each element
+        // "entry"
+        // convert the map to an entrySet
+        for (Entry<Character, Integer> entry : charCount.entrySet()) {
+            if (entry.getValue() == 1) {
+                System.out.println(entry.getKey());
+                return; // to stop it from returning more than 1 result
+            }
+        }
+
     }
 
-    // loop through Entry of chars & ints & call each element 'entry'
-    // convert map to entrySet
-    for (Entry<Character, Integer> entry : charCount.entrySet()) {
-      if (entry.getValue() == 1) {
-        System.out.println(entry.getKey());
-        return;
-      }
+    public static void main(String[] args) {
+        Practice p = new Practice();
+        p.firstNonRepeat("hi there");
     }
-  }
-
-  public static void main(String[] args) {
-    Practice p = new Practice();
-    p.firstNonRepeat("hi there");
-  }
 }
