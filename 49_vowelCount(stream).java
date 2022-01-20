3d6d0642138cf0c83c89ed86dd452390b467873a
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-01-20 08:32:40 -0600

    Update 49_vowelCount(stream).java

--- 49_vowelCount(stream).java
+++ 49_vowelCount(stream).java
@@ -1,21 +1,17 @@
 import java.util.stream.*;
-import java.util.*; // imports Arrays
+import java.util.Map.Entry;
+import java.util.*;
 
 public class Practice {
-    public void vowelCount(String str) {
-        List<String> vowels = Arrays.asList("a", "e", "i", "o", "u");
+    public static void main(String[] args) {
+        List<String> vowels = List.of("a", "e", "i", "o", "u");
 
-        // generate stream from an String[] of single character strings
-        long count = Arrays.stream(str.split("")) 
-                .filter(vowels::contains) // remove all non-vowels
+        String str = "hello";
+
+        long count = Stream.of(str.split(""))
+                .filter(vowels::contains)
                 .count();
 
         System.out.println(count);
-    }
-
-    public static void main(String[] args) {
-        Practice p = new Practice();
-
-        p.vowelCount("hiii");
     }
 }
