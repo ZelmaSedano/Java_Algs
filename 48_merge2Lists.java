5bc645af75dda42eab278276517abb8db3a7bbb4
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-01-20 08:24:21 -0600

    Update 48_merge2Lists.java

--- 48_merge2Lists.java
+++ 48_merge2Lists.java
@@ -7,9 +7,9 @@ public class Practice {
         List<Integer> list1 = List.of(1, 2, 3);
         List<Integer> list2 = List.of(4, 5, 6);
 
-        // merge the list using streams
-        List merged = Stream.concat(list1.stream(), list2.stream())
+        List<Integer> merged = Stream.concat(list1.stream(), list2.stream())
                 .collect(Collectors.toList());
+
         System.out.println(merged);
     }
 }
