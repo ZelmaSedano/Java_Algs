cb3b6f61844b240fe67ff53e60ea46a441e309ef
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-02-22 16:42:34 -0600

    Update 43_reverseString_long.java

--- 43_reverseString_long.java
+++ 43_reverseString_long.java
@@ -1,18 +1,12 @@
-import java.util.stream.*;
-import java.util.*; // imports List
+class practice{
+    // method
+    public String reverseStringShort(String str) {
+        return new StringBuilder(str).reverse().toString();
 
-public class Practice {
-    // create a method that reverses a given string
-    public String reverseStr(String str) {
-        // create a String varaible called "reverse"
-        // set it equal to new StringBuilder that reverses & converts back to string
-        String reversed = new StringBuilder(str).reverse().toString();
-        return reversed;
     }
 
-    public static void main(String[] args) {
-        Practice p = new Practice();
-
-        System.out.println(p.reverseStr("no"));
+    public static void main(String[] args)  {
+        practice p = new practice();
+        System.out.println(p.reverseStringShort("hi"));
     }
 }
